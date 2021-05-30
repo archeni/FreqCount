d7833a9c4fd71126ec10292e694b990df7f23b48
Traye <johnson.traye@yahoo.com>
2021-05-30 00:05:40 -0500

    finished translation

---
+++ word-frequency/csharp/word-frequency/FreqCounts.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace word_frequency
+{
+  class FreqCount
+  {
+    public Dictionary<string, int> wordCount = new Dictionary<string, int>();
+  }
+
+}
